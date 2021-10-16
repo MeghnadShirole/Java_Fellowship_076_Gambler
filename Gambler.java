d132c881b6bede83782bf2cc8cfee45dea98978f
MeghnadShirole <shirolemeghnad10@gmail.com>
2021-10-16 09:26:35 +0530

    Gambler_Problem

---
+++ Gambler.java
@@ -0,0 +1,26 @@
+	import java.util.Random;
+	import java.util.Scanner;
+public class Gambler {
+	
+
+
+	public static void main (String[] args) {
+//	Scanner sc = new Scanner(system.in);
+//	int $numberOfTimesToGamble
+//	System.out.println("Enter the number of times to gamble");
+//	$numberOfTimesToGamble=sc.nextInt();
+
+
+	int cash=$stack;
+
+	Random randomNumber=new Random();
+ 	double random = randomNumber.nextDouble();
+            if (random < 0.5)
+                cash++;
+             else    
+                cash--;
+
+
+}
+
+}
